c1e4e3797906b6c950aa2bc60b37e223a52e588a
Seven <seven@apowo.com>
2019-03-15 18:46:55 +0800

    init

---
+++ lib/config.ts
@@ -0,0 +1,57 @@
+// 配置账号信息
+export interface Config {
+    /**
+     * 修改这里配置为您自己申请的商户信息
+     * 微信公众号信息配置
+     *
+     * APPID：绑定支付的APPID（必须配置，开户邮件中可查看）
+     *
+     * MCHID：商户号（必须配置，开户邮件中可查看）
+     */
+    GetAppId(): string;
+
+    GetMerchantId(): string;
+
+    /**
+     * 支付回调url
+     * 签名和验证签名方式， 支持md5和sha256方式
+     */
+    GetNotifyUrl(): string;
+
+    GetSignType(): string;
+
+    /**
+     * 接口调用上报等级，默认紧错误上报（注意：上报超时间为【1s】，上报无论成败【永不抛出异常】，
+     * 不会影响接口调用流程），开启上报之后，方便微信监控请求调用的质量，建议至少
+     * 开启错误上报。
+     * 上报等级，0.关闭上报; 1.仅错误出错上报; 2.全量上报
+     */
+    GetReportLevenl(): number;
+
+    /**
+     * KEY：商户支付密钥，参考开户邮件设置（必须配置，登录商户平台自行设置）, 请妥善保管， 避免密钥泄露
+     * 设置地址：https://pay.weixin.qq.com/index.php/account/api_cert
+     *
+     * APPSECRET：公众帐号secert（仅JSAPI支付的时候需要配置， 登录公众平台，进入开发者中心可设置）， 请妥善保管， 避免密钥泄露
+     * 获取地址：https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=2005451881&lang=zh_CN
+     */
+    GetKey(): string;
+
+    GetAppSecret(): string;
+
+    /**
+     * 设置商户证书路径
+     * 证书路径,注意应该填写绝对路径（仅退款、撤销订单时需要，可登录商户平台下载，
+     * API证书下载地址：https://pay.weixin.qq.com/index.php/account/api_cert，下载之前需要安装商户操作证书）
+     * 注意:
+     * 1.证书文件不能放在web服务器虚拟目录，应放在有访问权限控制的目录中，防止被他人下载；
+     * 2.建议将证书文件名改为复杂且不容易猜测的文件名；
+     * 3.商户服务器要做好病毒和木马防护工作，不被非法侵入者窃取证书文件。
+     */
+    GetSSLCertPath(): SSLCert;
+}
+
+export interface SSLCert {
+    sslCertPath: string;
+    sslKeyPath: string;
+}
